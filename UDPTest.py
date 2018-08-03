d3b39b47fe1ff79f424d0d127219f2b649c94548
sam <bluemug4@gmail.com>
2018-08-03 11:57:51 -0700

    Added UDPTest script

---
+++ UDPTest.py
@@ -0,0 +1,16 @@
+import socket
+
+HOST = '10.100.15.212'  # Standard loopback interface address (localhost)
+PORT = 3100        # Port to listen on (non-privileged ports are > 1023)
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket.bind((HOST, PORT))
+
+while True:
+    data, addr = server_socket.recvfrom(1024)
+    print('Connected by', addr)
+    print("recevied message: ", data)
+
+    if data == b'\x8f\x8f\x8f\x8f\xaa\xbb\xcc\xdd\xee\xff\x00\x00\x00\x00\x06\x00\xff':
+        print('here')
+        server_socket.sendto(b'\x8f\x8f\x8f\x8f\xAA\xbb\xcc\xdd\xee\xff\x01\x00\x00\x00\x06\x01\x01\xff', addr)
