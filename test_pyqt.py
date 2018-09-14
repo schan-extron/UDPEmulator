b1f5210523c47031c1bb01b006238f9a41b441aa
wqnguyen <wnguyen@extron.com>
2018-09-14 11:50:14 -0700

    -Added icons for user interface -Added state button generation

--- test_pyqt.py
+++ test_pyqt.py
@@ -17,11 +17,14 @@ class App(QMainWindow):
         self.initUI()
         self.IPAddr = socket.gethostbyname(socket.gethostname())
         self.port = '0'
+        self.ethernetType = ''
+        self.commandStateDict = {}
+        self.commandStateButtonMap = {}
 
     def initUI(self):
         self.setWindowTitle(self.title)
-        self.setGeometry(300, 300, 720, 480)
-        self.setWindowIcon(QIcon("icon.PNG"))
+        self.setGeometry(300, 300, 1200, 800)
+        self.setWindowIcon(QIcon("icons\\icon.PNG"))
         self.statusBar().showMessage('Ready')
         self.setAcceptDrops(True)
         self.widget = QWidget()
@@ -36,13 +39,62 @@ class App(QMainWindow):
 
     def createSettings(self):
         self.setAcceptDrops(False)
-        self.commandsVBox = QVBoxLayout()
-        self.rightSide = QVBoxLayout()
-        cmdLabelList = [QLabel(cmd) for cmd in self.listOfCommands]
-        for c in cmdLabelList:
-            c.setFixedWidth(300)
-            self.commandsVBox.addWidget(c)
-            self.commandsVBox.addWidget(QLineEdit())
+
+        # Create combo box for HTTP request type
+        self.dropdownBox = QComboBox()
+        self.dropdownBox.addItems(['GET', 'POST', 'PUT'])
+
+
+
+
+        # Container Widget
+        widget = QWidget()
+        # Layout of Container Widget
+        commandsVBox = QVBoxLayout(self)
+        buttonsLayoutWidget = QGridLayout()
+
+        for command in self.listOfCommands:
+            if type(self.commandStateDict[command]) is dict:
+                print(command + ' has a dict')
+            else:
+                commandLabel = QLabel(command)
+                commandLabel.setFixedWidth(300)
+                commandsVBox.addWidget(commandLabel)
+                #commandsVBox.addWidget(QLineEdit())
+
+                tempStateList = self.commandStateDict[command]
+
+                buttonGroup = QButtonGroup()
+                buttonGroup.setExclusive(True)
+                for state in range(0, len(tempStateList)):
+                    button = QPushButton(tempStateList[state], checkable=True)
+                    self.saveStateButton(command + tempStateList[state], button)
+
+                    buttonGroup.addButton(button)
+                    button.clicked.connect(lambda: self.stateButtonClicked())
+                    commandsVBox.addWidget(button)
+
+                #commandsVBox.addWidget(buttonGroup)
+                #buttonGroup.buttonClicked(lambda: self.stateButtonClicked())
+        self.show()
+
+
+
+
+        widget.setLayout(commandsVBox)
+
+        # Scroll Area Properties
+        scroll = QScrollArea()
+        scroll.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        scroll.setWidgetResizable(False)
+        scroll.setWidget(widget)
+
+        # Scroll Area Layer add
+        vLayout = QVBoxLayout(self)
+        vLayout.addWidget(scroll)
+        self.setLayout(vLayout)
+
 
         ipLineEdit = QLineEdit()
         ipLineEdit.setText(self.IPAddr)
@@ -68,24 +120,18 @@ class App(QMainWindow):
         connectionVbox.addStretch(1)
         settingHBox.addLayout(flo)
         settingHBox.addLayout(connectionVbox)
+
+
+
+
+        self.rightSide = QVBoxLayout()
         self.rightSide.addLayout(settingHBox)
-        self.hbox.addLayout(self.commandsVBox)
+
+        self.hbox.addLayout(vLayout)
+
+        #self.hbox.addLayout(self.commandsVBox)
         self.hbox.addSpacing(15)
         self.hbox.addLayout(self.rightSide)
-
-        addCommandAction = QAction(QIcon('add.png'), 'Add Command', self)
-        addCommandAction.setShortcut('Ctrl+1')
-        addCommandAction.setStatusTip('Add Command')
-        addCommandAction.triggered.connect(self.addCommandClicked)
-
-        saveFileAction = QAction(QIcon('save.png'), 'Save', self)
-        saveFileAction.setShortcut('Ctrl+2')
-        saveFileAction.setStatusTip('Save current emulator file')
-        saveFileAction.triggered.connect(self.saveFileClicked)
-
-        toolbar = self.addToolBar('Add Command')
-        toolbar.addAction(addCommandAction)
-        toolbar.addAction(saveFileAction)
 
         traceVBox = QVBoxLayout()
         w = QWidget()
@@ -103,6 +149,49 @@ class App(QMainWindow):
         traceVBox.addWidget(transmitBox)
         self.rightSide.addLayout(traceVBox)
 
+        self.initializeToolBar()
+
+    def initializeToolBar(self):
+        # Create Add Button
+        #########################################################################################################
+        addCommandAction = QAction(QIcon('icons\\add.png'), 'Add Command', self)
+        addCommandAction.setShortcut('Ctrl+1')
+        addCommandAction.setStatusTip('Add Command')
+        addCommandAction.triggered.connect(self.addCommandClicked)
+
+        # Create Delete Button
+        #########################################################################################################
+        removeCommandAction = QAction(QIcon('icons\\remove.png'), 'Remove Command', self)
+        removeCommandAction.setShortcut('Ctrl+2')
+        removeCommandAction.setStatusTip('Remove Command')
+        removeCommandAction.triggered.connect(self.removeCommandClicked)
+
+        # Create Open Button
+        #########################################################################################################
+        openFileAction = QAction(QIcon('icons\\open.png'), 'Open Emulator File', self)
+        openFileAction.setShortcut('Ctrl+3')
+        openFileAction.setStatusTip('Open Emulator File')
+        openFileAction.triggered.connect(self.openFileClicked)
+
+        # Create Save Button
+        #########################################################################################################
+        saveFileAction = QAction(QIcon('icons\\save.png'), 'Save', self)
+        saveFileAction.setShortcut('Ctrl+4')
+        saveFileAction.setStatusTip('Save Emulator File')
+        saveFileAction.triggered.connect(self.saveFileClicked)
+
+        toolbar = self.addToolBar('Add Command')
+        toolbar.addAction(addCommandAction)
+        toolbar.addAction(removeCommandAction)
+        toolbar.addAction(openFileAction)
+        toolbar.addAction(saveFileAction)
+
+    def stateButtonClicked(self):
+        print(self.sender().text())
+
+    def saveStateButton(self, keyName, buttonObject):
+        self.commandStateButtonMap[keyName] = buttonObject
+
     def connectClicked(self):
         self.statusBar().showMessage("Connected")
 
@@ -114,8 +203,14 @@ class App(QMainWindow):
         newCmdLineEdit.setFixedWidth(300)
         self.commandsVBox.addWidget(newCmdLineEdit)
 
+    def removeCommandClicked(self):
+        pass
+
+    def openFileClicked(selfs):
+        pass
+
     def saveFileClicked(self):
-        print('saved')
+        pass
 
     def dragEnterEvent(self, event):
         if event.mimeData().hasText():
@@ -130,12 +225,22 @@ class App(QMainWindow):
 
         json_parser_obj = customJsonParser(data)
         json_parser_obj.storeModelsAndCommands()
+        json_parser_obj.storePortAndType()
 
-        self.listOfCommands = list(json_parser_obj.commands.keys())
-        print('here', self.listOfCommands)
-        print(data)
-        self.port = str(data['Protocols'][1]['Port'])
-        print(self.port)
+
+        self.commandStateDict = json_parser_obj.commands
+        # Remove Connection Status key
+        del self.commandStateDict['Connection Status']
+        print(self.commandStateDict)
+        self.listOfCommands = list(self.commandStateDict.keys())
+        self.port = json_parser_obj.port
+        self.ethernetType = json_parser_obj.ethernetType
+
+        if self.ethernetType == 'HTTP':
+            self.setWindowTitle('HTTP Emulator')
+        else:
+            self.setWindowTitle('UDP Emulator')
+
         self.label.clear()
         self.createSettings()
 
